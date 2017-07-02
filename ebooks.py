d89b5ccc87c0c172f1e1b06bffdeab1e35c92425
Cotter Koopman <Cotter@Cotters-MacBook-Air.local>
2017-07-02 14:35:27 -0400

    Trying reading board from file and printing

--- ebooks.py
+++ ebooks.py
@@ -11,21 +11,21 @@ def connect():
                           access_token_secret=MY_ACCESS_TOKEN_SECRET)
     return api
 
-# def getBoardFromFile(statefilename):
-#     f = open(statefilename, 'r+')
-#     f.readline()
-#     i = 0
-#     board = [[0 for i in range(8)] for j in range(8)]
-#     for line in f:
-#         if int(line) == 1:
-#             board[i%8][i//8] = '⚫️'
-#         elif int(line) == 0:
-#             board[i%8][i//8] = '⚪️'
-#         else:
-#             board[i%8][i//8] = '🔴' #for error handling
-#         i+=1
-#
-#     f.close()
+def getBoardFromFile(statefilename):
+    f = open(statefilename, 'r+')
+    f.readline()
+    i = 0
+    board = [[0 for i in range(8)] for j in range(8)]
+    for line in f:
+        if int(line) == 1:
+            board[i%8][i//8] = '⚫️'
+        elif int(line) == 0:
+            board[i%8][i//8] = '⚪️'
+        else:
+            board[i%8][i//8] = '🔴' #for error handling
+        i+=1
+    f.close()
+    return board
 
 def getStringFromBoard():
     boardstr = ""
@@ -40,7 +40,7 @@ if __name__=='__main__':
     api = connect()
 
     board = [[0 for i in range(8)] for j in range(8)]
-    #board = getBoardFromFile('state.txt')
+    board = getBoardFromFile('state.txt')
     boardstr = ''
     boardstr = getStringFromBoard()
 
