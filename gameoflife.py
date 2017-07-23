a4eab6f389b2f5dfafe4dd40e1583fe2709322b1
Cotter Koopman <cjk45@students.calvin.edu>
2017-07-23 18:57:28 -0400

    Added comments

--- gameoflife.py
+++ gameoflife.py
@@ -2,8 +2,10 @@ import random
 import re
 import sys
 import twitter
+#Get tokens and keys to connect to the right account
 from local_settings import *
 
+#Connect to twitter API
 def connect():
     api = twitter.Api(consumer_key=MY_CONSUMER_KEY,
                           consumer_secret=MY_CONSUMER_SECRET,
@@ -11,41 +13,46 @@ def connect():
                           access_token_secret=MY_ACCESS_TOKEN_SECRET)
     return api
 
+#Parses previous board tweet into string of Xs and _s, then 2D array of 1s and 0s
 def getBoardFromText(boardtext):
-    board = [[0 for i in range(8)] for j in range(8)]
+    board = [[0 for i in range(8)] for j in range(8)] #create new blank board
     boardtext = re.sub(r'\n','', boardtext) #take out new lines.
     boardtext = re.sub(r'\"|\(|\)', '', boardtext) #take out quotes.
-    boardtext = boardtext.replace("🔵", "X")
-    boardtext = boardtext.replace("⚪️", "_")
+    boardtext = boardtext.replace("🔵", "X")    #live
+    boardtext = boardtext.replace("⚪️", "_")    #dead
     boardtextlist = list(boardtext)
-    # print(boardtextlist)
     for j in range(8):
         for i in range(8):
+            #live cell
             if (boardtextlist[i+(j*8)] == 'X'):
                 board[i][j] = 1
+            #dead cell
             elif (boardtextlist[i+(j*8)] == '_'):
                 board[i][j] = 0
+            #for debugging
             else:
                 board[i][j] = 5
     return board
 
+#Returns a count of how many of a cell's 8 neighbors are alive
+#Modular, so that board wraps
 def countNeighbors(x, y):
     count = 0
-    if (board[x][(y-1)%8]==1):
+    if (board[x][(y-1)%8]==1): #W
         count +=1
-    if (board[x][(y+1)%8]==1):
+    if (board[x][(y+1)%8]==1): #E
         count +=1
-    if (board[(x-1)%8][y]==1):
+    if (board[(x-1)%8][y]==1): #N
         count +=1
-    if (board[(x-1)%8][(y-1)%8]==1):
+    if (board[(x+1)%8][y]==1): #S
         count +=1
-    if (board[(x-1)%8][(y+1)%8]==1):
+    if (board[(x-1)%8][(y-1)%8]==1): #NW
         count +=1
-    if (board[(x+1)%8][y]==1):
+    if (board[(x-1)%8][(y+1)%8]==1): #SW
         count +=1
-    if (board[(x+1)%8][(y-1)%8]==1):
+    if (board[(x+1)%8][(y-1)%8]==1): #NE
         count +=1
-    if (board[(x+1)%8][(y+1)%8]==1):
+    if (board[(x+1)%8][(y+1)%8]==1): #SE
         count +=1
     return count
 
@@ -53,7 +60,9 @@ def countNeighbors(x, y):
 # Any live cell with two or three live neighbors lives on to the next generation.
 # Any live cell with more than three live neighbors dies, as if by over-population.
 # Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+# Returns live or dead cell
 def rules(x, y):
+    #If alive...
     if(board[x][y]==1):
         if (countNeighbors(x, y) < 2):
             return 0
@@ -61,15 +70,17 @@ def rules(x, y):
             return 1
         elif (countNeighbors(x, y) > 3):
             return 0
+    #If dead...
     elif(board[x][y]==0):
         if (countNeighbors(x, y) == 3):
             return 1
         else:
             return 0
+    #For debugging
     else:
         return 5
 
-
+#Generates board string from 2D array of 1s and 0s
 def getStringFromBoard(b):
     bs = ""
     for j in range(8):
@@ -78,11 +89,13 @@ def getStringFromBoard(b):
                 bs += '🔵'
             elif b[i][j] == 0:
                 bs += '⚪️'
+            #For debugging
             else:
                 bs += '🔴'
         bs += '\n'
     return bs
 
+#Generate next board according to the rules at each cell
 def getNextGenFromBoard():
     nextboard = [[0 for i in range(8)] for j in range(8)]
     for j in range(8):
@@ -90,6 +103,7 @@ def getNextGenFromBoard():
             nextboard[i][j] = rules(i, j)
     return nextboard
 
+#For when a board becomes blank or locked
 def generateRandomBoard():
     randboard = [[0 for i in range(8)] for j in range(8)]
     for j in range(8):
@@ -100,6 +114,7 @@ def generateRandomBoard():
 if __name__=='__main__':
     api = connect()
 
+    #Retrieve last board genenration.
     boardtext = api.GetUserTimeline(screen_name='gameoflife_bot', count=1, max_id=None, include_rts=False, trim_user=True, exclude_replies=True)[0].text
     if '⚪️' not in boardtext:
         #Last board was a text message. Generate random board.
@@ -116,10 +131,13 @@ if __name__=='__main__':
     nextboard = getNextGenFromBoard()
     nextboardstr = getStringFromBoard(nextboard)
 
+    #No live cells in last board
     if '🔵' not in boardtext:
         nextboardstr = 'This population is extinct, so I\'ll generate a new board randomly.'
+    #Generation next generation produced an identical board, meaning the configuration is stable.
     if nextboardstr == boardstr:
         nextboardstr = 'Population is locked, so I\'ll generate a new board randomly.'
 
     print(nextboardstr)
+    #Tweet board
     status = api.PostUpdate(nextboardstr)
